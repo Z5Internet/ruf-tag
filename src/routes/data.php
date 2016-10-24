5fc22c633028f46e27d93004a51c43e5560b3841
Darren <darren@dmerrett.com>
2016-10-24 21:30:02 +0100

    fix case error in route controller

--- src/routes/data.php
+++ src/routes/data.php
@@ -2,10 +2,10 @@
 
 Route::group(['prefix' => 'data/tags'], function () {
 
-	Route::post('/addBatch', 'darrenmerrett\ReactTag\App\Http\Controllers\routesController@addBatch');
+	Route::post('/addBatch', 'darrenmerrett\ReactTag\App\Http\Controllers\RoutesController@addBatch');
 
-	Route::post('/addTag', 'darrenmerrett\ReactTag\App\Http\Controllers\routesController@addTag');
+	Route::post('/addTag', 'darrenmerrett\ReactTag\App\Http\Controllers\RoutesController@addTag');
 
-	Route::get('/getBatchs', 'darrenmerrett\ReactTag\App\Http\Controllers\routesController@getBatchs');
+	Route::get('/getBatchs', 'darrenmerrett\ReactTag\App\Http\Controllers\RoutesController@getBatchs');
 	
 });
